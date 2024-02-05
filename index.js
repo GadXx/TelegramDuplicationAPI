22c93000864b61e03c190bba7080a2d3d513535a
GadXx <dd94307@gmail.com>
2024-02-05 13:38:46 +0300

    123

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import TelegramBot from 'node-telegram-bot-api';
+
+const app = express();
+const port = 3000; 
+
+app.use(bodyParser.json());
+
+// Токен бота Telegram
+const botToken = '6769689712:AAHZEeL9MwkKuLTi22SSFOCWM-SKKfmNVyA';
+const bot = new TelegramBot(botToken, { polling: true });
+
+
+app.post('/api/duplicateContact', (req, res) => {
+
+    const { name, contact } = req.body;
+
+    if (!name || !contact) {
+        return res.status(400).json({ error: 'Both name and contact are required.' });
+    }
+
+    // Название чата куда пересылаются контактные данные
+    const chatId = '@MirrorEcho'; 
+    const message = `Имя: ${name}\n Контакт: ${contact}`;
+
+    bot.sendMessage(chatId, message)
+        .then(() => {
+            res.status(200).json({ success: true });
+        })
+        .catch((error) => {
+            console.error('Error sending message to Telegram:', error);
+            res.status(500).json({ error: 'Internal server error' });
+        });
+    });
+
+app.listen(port);
